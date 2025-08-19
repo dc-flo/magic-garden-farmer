d300be9f03655519b7af76248a745aedcf764e42
flo <flo.joh.rock@gmail.com>
2025-08-19 23:19:16 +0200

    init commit

---
+++ script.py
@@ -0,0 +1,65 @@
+import time
+from time import sleep
+
+import keyboard, pygetwindow
+
+def sell():
+    keyboard.press_and_release("shift+3")
+    keyboard.press_and_release("space")
+
+def harvest():
+    if keyboard.is_pressed("q"):
+        exit()
+    for i in range(5):
+        keyboard.press_and_release('space')
+
+def go_left():
+    keyboard.press_and_release('a')
+    sleep(.1)
+
+def go_right():
+    keyboard.press_and_release('d')
+    sleep(.1)
+
+def go_up():
+    keyboard.press_and_release('w')
+    sleep(.1)
+
+def go_down():
+    keyboard.press_and_release('s')
+    sleep(.1)
+
+def reset(x, y):
+    keyboard.press_and_release('shift+2')
+    for i in range(10 - x):
+        go_left()
+    for j in range(10 - y):
+        go_up()
+
+def farm(x, y):
+    for j in range(y):
+        for i in range(x-1):
+            harvest()
+            if j % 2 == 0:
+                go_right()
+            else:
+                go_left()
+        harvest()
+        go_down()
+
+
+if __name__ == '__main__':
+    w = pygetwindow.getWindowsWithTitle("Chrome")[0]
+    w.activate()
+
+    old = time.time_ns()
+
+    while True:
+        reset(0, 5)
+        farm(21, 5)
+        sell()
+
+
+        curr = time.time_ns()
+        print(curr - old)
+        old = curr
